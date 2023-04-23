8a0ceb0b5a015965e3bcc0a43cdcec56620c2084
sivilabdullah <194308008@ogr.ebyu.edu.tr>
2023-04-23 13:57:59 +0300

    first commit

--- timer usage example/ViewController.swift
+++ timer usage example/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var timeCounter: UILabel!
+    var timer = Timer()
+    var counter = 30
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
     }
-
-
+    @objc func timerFunction(){
+        if counter <= 30 && counter > 10  {
+            counter -= 1
+            timeCounter.text = "\(counter)"
+            imageView.image = UIImage(named: "bomb")
+            timeCounter.textColor = .black
+        }else if counter <= 10 && counter > 0  {
+            counter -= 1
+            timeCounter.text = "\(counter)"
+            timeCounter.textColor = .red
+        }else{
+            imageView.image = UIImage(named: "explosion")
+        }
+    }
+    
+    
 }
 
